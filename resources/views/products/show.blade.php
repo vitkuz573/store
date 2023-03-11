59be7c4649c12b419653e71416ff09284168c633
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-03-11 23:36:12 +0500

    UPD

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -3,24 +3,24 @@
 @section('content')
     <div class="container py-5">
         <div class="row justify-content-center align-items-center">
-            <div class="col-lg-5">
-                <img src="{{ $product->image_url }}" class="w-100 rounded-3 shadow-lg mb-4" alt="{{ $product->name }} image">
+            <div class="col-lg-5 mb-4 mb-lg-0">
+                <img src="{{ $product->image_url }}" class="w-100 rounded-3 shadow-lg" alt="{{ $product->name }} image">
             </div>
             <div class="col-lg-7">
-                <div class="bg-white rounded-3 shadow-lg p-5">
+                <div class="bg-white rounded-3 shadow-lg p-5 h-100">
                     <h1 class="text-primary mb-4">{{ $product->name }}</h1>
                     @if ($product->is_new)
-                        <div class="badge bg-success mb-4">NEW!</div>
+                        <span class="badge bg-success mb-4">NEW!</span>
                     @endif
                     <p class="text-muted mb-4">{{ $product->description }}</p>
                     <div class="d-flex justify-content-between align-items-center mb-4">
                         <h2 class="text-primary mb-0">{{ $product->price }} руб.</h2>
-                        <form action="{{ route('cart.add', $product) }}" method="POST">
+                        <form method="POST" action="{{ route('cart.add', $product) }}">
                             @csrf
                             <button type="submit" class="btn btn-primary px-4 rounded-pill">Add to Cart</button>
                         </form>
                     </div>
-                    <a href="{{ route('products.index') }}" class="btn btn-outline-primary px-4 rounded-pill">Back to Products</a>
+                    <a href="{{ route('products.index') }}" class="btn btn-outline-primary px-4 rounded-pill mt-auto">Back to Products</a>
                 </div>
             </div>
         </div>
@@ -39,20 +39,14 @@
         }
 
         .btn-primary {
-            background-color: #4CAF50;
-            border-color: #4CAF50;
             transition: all 0.2s;
         }
 
         .btn-primary:hover {
-            background-color: #43A047;
-            border-color: #43A047;
             transform: translateY(-2px);
         }
 
         .btn-outline-primary {
-            border-color: #4CAF50;
-            color: #4CAF50;
             transition: all 0.2s;
         }
 
@@ -80,6 +74,10 @@
         .text-primary {
             font-size: 2rem;
             font-weight: bold;
+        }
+
+        .h-100 {
+            height: 100%;
         }
     </style>
 @endsection

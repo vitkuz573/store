51bde55bd1fd9bde2d0e8f2da024b9ce2d2a7263
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-03-11 22:24:23 +0500

    UPD

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -16,11 +16,11 @@
                     </div>
                     <div class="card-body p-3">
                         <h5 class="card-title fw-bold text-uppercase mb-2">{{ $product->name }}</h5>
-                        <p class="card-text text-muted">{{ Str::limit($product->description, 100) }}</p>
                         <div class="d-flex align-items-center justify-content-between">
                             <p class="card-text text-primary fw-bold h5 mb-0">{{ $product->price }} руб.</p>
                             <a href="{{ route('products.show', $product) }}" class="btn btn-primary">Подробнее</a>
                         </div>
+                        <p class="card-text text-muted mt-2">{{ Str::limit($product->description, 100) }}</p>
                     </div>
                 </div>
             </div>
@@ -31,4 +31,23 @@
     <div class="d-flex justify-content-center mt-4">
         {{ $products->links() }}
     </div>
+@endsection
+
+@section('styles')
+    <style>
+        .card:hover {
+            box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+        }
+
+        .card-title {
+            font-size: 1.2rem;
+            margin-bottom: 0.5rem;
+        }
+
+        .card-body {
+            position: relative;
+            padding: 2rem;
+            background-color: #fff;
+        }
+    </style>
 @endsection

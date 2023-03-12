a1a83c49668fe105f20fe90becb54c5986bab22a
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-03-12 17:27:15 +0500

    Добавлен мидлвар AdminOnly

---
+++ app/Http/Middleware/AdminOnly.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminOnly
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->user() || !$request->user()->roles()->whereName('admin')->exists()) {
+            abort(403, 'Access denied');
+        }
+
+        return $next($request);
+    }
+}
